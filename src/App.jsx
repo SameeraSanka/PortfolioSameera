a606b53492e150a4577ba86168db59a3b69fa394
Sameera Madusanka <109691387+SameeraSanka@users.noreply.github.com>
2024-01-18 22:06:09 +0530

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <About />
+      {/* <About />
       <Work />
       <Testimonial />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
